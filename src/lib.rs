09cb9c83403b3ae02e6810666b5ef349c7fd33c3
dgrubb <davidanthonygrubb@gmail.com>
2019-09-16 12:18:40 -0500

    Cleanup skeleton implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,50 @@
 //! TODO: Complete this documentation
+use std::result::Result::{self, Ok, Err};
 
-pub struct PatchEntry<> {
-    offset: u32,
-    length: u32,
-    is_rle: bool,
-    payload: Vec<u8>
-}
+mod rips {
+    pub struct PatchEntry {
+        offset: u32,
+        length: u32,
+        is_rle: bool,
+        payload: Vec<u8>
+    }
 
-/// Determines the index of an IPS header:
-/// "PATCH" (50, 41, 54, 43, 48)
-/// Returns an index of 0 if no header was found.
-pub fn verify_header_index(data: Vec<u8>) -> u32 {
-    0
-}
+    /// Determines the index of an IPS header:
+    /// "PATCH" (50, 41, 54, 43, 48)
+    /// Returns an index of 0 if no header was found.
+    pub fn verify_header_index(_data: Vec<u8>) -> u32 {
+        0
+    }
 
-/// Determines the index of an IPS end-of-file marker:
-/// "EOF" (45 4f 46)
-/// Returns an index of 0 if no header was found.
-pub fn verify_eof_marker(data: Vec<u8>) -> u32 {
-    0
-}
+    /// Determines the index of an IPS end-of-file marker:
+    /// "EOF" (45 4f 46)
+    /// Returns an index of 0 if no header was found.
+    pub fn verify_eof_marker(_data: Vec<u8>) -> u32 {
+        0
+    }
 
-/// Scans raw patch file data (list of bytes) and converts 
-/// each encountered patch instruction sequence into a struct
-/// representing the change.
-/// Returns a list of patches.
-pub fn get_patch_list(data: Vec<u8>) -> Vec<PatchEntry> {
-}
+    /// Scans raw patch file data (list of bytes) and converts
+    /// each encountered patch instruction sequence into a struct
+    /// representing the change.
+    /// Returns a list of patches.
+    pub fn get_patch_list(_data: Vec<u8>) -> Result<Vec<PatchEntry>, String> {
+        Err("This function requires completion".to_string())
+    }
 
-/// Applies a list of patches to raw patch file data.
-/// Returns a copy of the input data with patches applied.
-pub fn apply_patch_list(data: Vec<u8>, patches: Vec<PatchEntry>) -> Vec<u8> {
-}
+    /// Applies a list of patches to raw patch file data.
+    /// Returns a copy of the input data with patches applied.
+    pub fn apply_patch_list(_data: Vec<u8>, _patches: Vec<PatchEntry>) -> Result<Vec<u8>, String> {
+        Err("This function requires completion".to_string())
+    }
 
-/// Pretty prints a list of PatchEntry instances.
-pub fn print_patch_list(data: Vec<PatchEntry>) {
-    
-}
+    /// Pretty prints a list of PatchEntry instances.
+    pub fn print_patch_list(_data: Vec<PatchEntry>) {
 
-fn apply_patch(data: Vec<u8>, patch: PatchEntry) {
+    }
 
+    fn apply_patch(_data: Vec<u8>, _patch: PatchEntry) {
+
+    }
 }
 
 #[cfg(test)]
@@ -49,3 +54,4 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
